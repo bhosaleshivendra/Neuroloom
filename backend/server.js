d24a2d523f349152c9976d3610a2b105aff9ebb1
bhosaleshivendra <bhosaleshivendra274@gmail.com>
2026-07-12 11:01:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,3 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+    res.send("Backend is working");
+});
