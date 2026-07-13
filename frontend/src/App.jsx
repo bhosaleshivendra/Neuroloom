21265a78a9eff4047c2964579b2860a596e0a9e5
bhosaleshivendra <bhosaleshivendra274@gmail.com>
2026-07-13 12:55:46 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,13 +10,13 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 const App = () => {
   return (
     <Router>
-      <div className="flex h-screen">
+      <div className="flex h-screen overflow-hidden">
         <Sidebar />
 
-        <div className="flex-1 flex flex-col">
+        <div className="flex-1 flex flex-col overflow-hidden">
           <Header />
 
-          <main className="p-6 flex-1">
+          <main className="p-6 flex-1 overflow-y-auto">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/employees" element={<Employees />} />
