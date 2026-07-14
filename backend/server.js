168a3097fbac72ad800a7c97e6b0e09e298a86bd
bhosaleshivendra <bhosaleshivendra274@gmail.com>
2026-07-14 21:35:40 +0530

    database connected

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,34 @@
 const express = require("express");
 const dotEnv = require('dotenv')
 const mongoose = require('mongoose')
+const dns = require('dns');
+const User = require("./models/User");
+
+dns.setServers([
+        '1.1.1.1',
+    '8.8.8.8'
+]);
 
 const app = express();
 
 app.use(express.json());
 dotEnv.config()
-console.log(process.version);
-console.log(process.env.MONGODB_URL);
-mongoose.connect(process.env.MONGODB_URL).then(() => console.log("MongoDB Connected"))
+
+mongoose.connect(process.env.MONGODB_URL)
+  .then(async () => {
+    console.log("MongoDB Connected");
+
+    await User.create({
+      name: "Shivendra",
+      email: "shivendra@example.com",
+    });
+
+    console.log("User inserted");
+  })
   .catch((err) => console.error(err));
 
-app.get("/", (req, res) => {
-    res.send("Backend is working");
-});
 
-
-
-const PORT = process.env.PORT || 3000;
+const PORT =3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
